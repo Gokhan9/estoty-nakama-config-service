67042a28256706d37a51ab0f3163c0cc0d2029e3
Gokhan9 <gcancomert@hotmail.com>
2026-04-26 17:17:21 +0300

    The configuration file has been added, and the nakama package has been imported.

---
+++ nakama/modules/rpc/config.go
@@ -0,0 +1,21 @@
+package rpc
+
+import (
+	"context"
+	"database/sql"
+	"encoding/json"
+
+	"github.com/heroiclabs/nakama-common/runtime"
+)
+
+func config(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, payload string) (string, error) {
+
+	config := map[string]interface{}{
+		"welcome_message": "Welcome to the game!",
+		"xp_rate":         1.5,
+		"rarity_options":  []string{"common", "rare", "epic", "legendary"},
+	}
+
+	bytes, _ := json.Marshal(config)
+	return string(bytes), nil
+}
